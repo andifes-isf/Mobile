292d3eae5f7793816735e545e488daec9723be13
LucasMBalieiro <lucas.macbali@gmail.com>
2025-03-05 14:46:38 -0300

    Readme completo

--- README.md
+++ README.md
@@ -1,10 +1,55 @@
+## Mobile - RedeIsF
+Este projeto foi realizado utilizando ReactNative como principal tecnologia para o desenvolvimento de um aplicativo mobile para a comunicação com os serviços do Idiomas sem Fronteiras. O usuário principal é o aluno IsF, porém todos os cargos poderão utilizar o mesmo aplicativo para realizarem suas tarefas
+
 ## Pré-requisitos
 
 - [git](https://git-scm.com/)
 - [NodeJS](https://nodejs.org/en/)
 - [npm](https://www.npmjs.com/)
+- [Backend](https://github.com/Arata2703/backend_projeto_andifes)
+- [AndroidStudio](https://developer.android.com/studio) Todos os testes foram realizados em dispositivos android
+- [Expo](https://expo.dev/) Pode ser instalado no seu próprio celular caso queira testar nele.
+
+Compatível com a última versão de todas as tecnologias, atualizado pela ultima vez em Fevereiro de 2025.
 
 ## Para compilar o projeto
 
+Certifique-se de possuir todos os pré-requisitos instalados, esteja com alguma instância Android ou IOS com Expo conectado na mesma rede que seu computador.
+
+#### Na pasta backend do projeto:
+1. `Siga a documentação do projeto`
+2. `Faça seu próprio arquivo .env`
+3. `Certifique-se de que a conexão esteja sendo feito no canal 8800`
+
+
+#### Na pasta do projeto mobile
 1. `npm install`
 2. `npm start`
+3.  `Selecione qual dispositivo deve se conectar pela CLI do Expo`
+
+## Login de usuário
+
+A primeira tela do sistema é a de login, e é necessário fazer a certificação de usuário com uma das contas existentes no banco de teste, segue alguns possíveis usuários:
+
+- Login: `Balla`	Role: `alunoisf`
+- Login: `Bruno` Role: `alunoisf`
+- Login: `Carlos` Role: `professorisf`
+- Login: `Enio` Role: `docenteministrante`
+
+Em todos os casos, a senha do usuário será `senha`
+
+## Descrição das telas
+
+- `Curso.tsx`: Demonstra a turma atual que o aluno IsF participa, retorna dados como nome, trilha, componente. Possui botões para a tela de CursoFeedback e para CursoHistoricos
+- `CursoFeedback.tsx`: Tela em que o usuário pode enviar sua avaliação sobre o ensino da matéria, recolhe uma lista json que será contabilizada como o feedback do aluno.
+- `CursoHistoricos.tsx`: Retorna uma lista de histórico de todas as turmas que este aluno já participou, ele também pode trocar a visão de acordo com o componente curricular que cada turma aborda.
+- `Login.tsx`: Tela inicial, possui requisição dos dados de login de usuário e armazena o token de certificação para as próximas telas.
+- `Ofertas.tsx`: Retorna a lista de ofertas disponíveis no momento, possui uma filtragem de acordo com o idioma no momento, porém deve ser trocado para uma filtragem de proficiência.
+- `Ouvidoria.tsx`: Canal de comunicação direta com a coordenação do curso, enviará um json diretamente para os usuários que possuirem este cargo, deve possuir a opção de envio anônimo ou não.
+- `Perfil.tsx`: Perfil do usuário, retorna suas informações pessoais como email, telefone, nome completo e login, também retorna dados acadêmicos como nível de proficiência no idioma, documento comprobatório e horas totais de curso, também retorna dados sobre sua instituição de ensino e possui um botão para entrar na tela de edição de dados.
+- `PerfilEdit.tsx`: Possibilita o usuário modificar seus dados pessoais, sendo eles email, telefone, etnia, gênero e nome.
+- `RelatorioEdit.tsx`: Possibilita a edição do relatório caso o orientador não tenha aprovado a entrega anterior.
+- `RelatorioHistorico.tsx`: Listagem de todos os relatórios que este usuário enviou, possui uma caixa de status da entrega (aprovado, pendente, reprovado) e um botão para abrir a edição daquela entrega.
+- `RelatorioOrientacao.tsx`: Tela de envio do relatório de orientação, possui os dados do orientador responsável no fim da página.
+- `RelatorioPratico.tsx`: Tela de envio do relatório prático, também possui os dados do orientador responsável no fim da página, possui um botão para o histórico de entregas do usuário.
+- `Turmas.tsx`: Tela embrionária, não é mais utilizada.
--- src/screens/RelatorioOrientacao.tsx
+++ src/screens/RelatorioOrientacao.tsx
@@ -1,164 +1,187 @@
-import { StyleSheet, Text, View, TouchableOpacity, PixelRatio, TextInput } from 'react-native';
-import React from 'react';
-import { useState } from 'react';
-import { SelectList } from 'react-native-dropdown-select-list';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  PixelRatio,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { useState } from "react";
+import { SelectList } from "react-native-dropdown-select-list";
 
-
-import { Feather } from '@expo/vector-icons';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Feather } from "@expo/vector-icons";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function RelatorioOrientacao() {
-
-
   const [avaliacao, setAvaliacao] = React.useState("");
-  //const [focoAvaliacao, setfocoAvaliacao] = React.useState('EN');  
+  //const [focoAvaliacao, setfocoAvaliacao] = React.useState('EN');
 
   const avaliacoes = [
-    {key:'1', value:'Péssima'},
-    {key:'2', value:'Ruim'},
-    {key:'3', value:'Regular'},
-    {key:'4', value:'Muito Boa'},
-    {key:'5', value:'Incrível'},
-  ]
+    { key: "1", value: "Péssima" },
+    { key: "2", value: "Ruim" },
+    { key: "3", value: "Regular" },
+    { key: "4", value: "Muito Boa" },
+    { key: "5", value: "Incrível" },
+  ];
 
   const [carga, setCarga] = React.useState("");
 
-  const cargas = [
-    {key:'1', value:'1 hora'},
-    {key:'2', value:'2 horas'},
-    {key:'4', value:'4 horas'},
-    {key:'8', value:'8 horas'},
-    {key:'16', value:'16 horas'},
-  ]
+  const cargas = [{ key: "1", value: "Discutir em Reunião" }];
 
-  const [form, setForm] = useState({ //Dados pra passar pra api do Rafa
-    avaliacao: '',
-    cargaHoraria: '',
-    descricao: '',
-  }); 
+  const [form, setForm] = useState({
+    //Dados pra passar pra api do Rafa
+    avaliacao: "",
+    cargaHoraria: "",
+    descricao: "",
+  });
 
-  
   return (
     <View style={styles.container}>
-      
-        <ScrollView>
-            <View style={[{paddingTop: '10%'}]}>
+      <ScrollView>
+        <View style={[{ paddingTop: "10%" }]}>
+          <View style={styles.linksHorizontais}>
+            <SelectList
+              setSelected={setAvaliacao}
+              data={avaliacoes}
+              placeholder="Avaliação"
+              search={false}
+              maxHeight={200}
+              arrowicon={
+                <Feather
+                  name={"chevron-down"}
+                  color={"#374957"}
+                  size={PixelRatio.getFontScale() * 20}
+                />
+              }
+              boxStyles={styles.selectBox}
+              inputStyles={styles.selectLabel}
+              dropdownStyles={{
+                zIndex: 1,
+                position: "absolute",
+                top: "100%",
+                width: "100%",
+                backgroundColor: "#D9D9D9",
+              }}
+            />
+            <SelectList
+              setSelected={setCarga}
+              data={cargas}
+              placeholder="Categoria"
+              search={false}
+              maxHeight={200}
+              arrowicon={
+                <Feather
+                  name={"chevron-down"}
+                  color={"#374957"}
+                  size={PixelRatio.getFontScale() * 20}
+                />
+              }
+              boxStyles={styles.selectBox}
+              inputStyles={styles.selectLabel}
+              dropdownStyles={{
+                zIndex: 1,
+                position: "absolute",
+                top: "100%",
+                width: "100%",
+                backgroundColor: "#D9D9D9",
+              }}
+            />
+          </View>
 
-                <View style={styles.linksHorizontais}>
-                    <SelectList
-                        setSelected={setAvaliacao}
-                        data={avaliacoes}
-                        placeholder="Avaliação"
-                        search={false}
-                        maxHeight={200}
-                        arrowicon={<Feather name={'chevron-down'} color={'#374957'} size={PixelRatio.getFontScale()*20}/>}
-                        boxStyles={styles.selectBox}
-                        inputStyles={styles.selectLabel}
-                        dropdownStyles={{zIndex: 1, position: 'absolute', top: '100%', width: '100%', backgroundColor: '#D9D9D9'}}
-                    />
-                    <SelectList
-                        setSelected={setCarga}
-                        data={cargas}
-                        placeholder="Carga Horária"
-                        search={false}
-                        maxHeight={200}
-                        arrowicon={<Feather name={'chevron-down'} color={'#374957'} size={PixelRatio.getFontScale()*20}/>}
-                        boxStyles={styles.selectBox}
-                        inputStyles={styles.selectLabel}
-                        dropdownStyles={{zIndex: 1, position: 'absolute', top: '100%', width: '100%', backgroundColor: '#D9D9D9'}}
-                    />
+          <View /*  CargaHoraria Observação */>
+            <View style={styles.containerEdit}>
+              <Text style={styles.labelEdit}>Carga Horária</Text>
+
+              <View style={{ flexDirection: "row" }}>
+                <View style={styles.boxInput}>
+                  <TextInput
+                    style={styles.labelInput}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    value={form.cargaHoraria}
+                    onChangeText={(cargaHoraria) =>
+                      setForm({ ...form, cargaHoraria })
+                    }
+                  />
                 </View>
 
-                <View /*  CargaHoraria Observação */>
-                    
-
-                    <View style={styles.containerEdit}>
-                        
-                        <Text style={styles.labelEdit}>Carga Horária</Text>
-
-                        <View style={{flexDirection:'row'}}>
-                            <View style={styles.boxInput}> 
-                                <TextInput
-                                style={styles.labelInput}
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                value={form.cargaHoraria}
-                                onChangeText={cargaHoraria => setForm({...form, cargaHoraria})}
-                                />
-                            </View>
-
-                            <Feather name = 'edit' color={'#374957'} size={PixelRatio.getFontScale()*24} style={{paddingLeft:PixelRatio.getFontScale() * 15, alignItems:'center', paddingTop: PixelRatio.getFontScale()*3}}/>
-
-                        </View>
-                        
-                    </View>
-
-                    <View style={styles.containerEdit}>
-
-                        <View style={styles.boxInputLong}> 
-                                    <TextInput
-                                    style={styles.labelInputBox}
-                                    autoCapitalize='none'
-                                    autoCorrect={false}
-                                    placeholder='Faça alguma observação'
-                                    value={form.descricao}
-                                    onChangeText={descricao => setForm({...form, descricao})}
-                                    />
-                        </View>
-                    </View>
-                    
-
-
-                </View>
-
+                <Feather
+                  name="edit"
+                  color={"#374957"}
+                  size={PixelRatio.getFontScale() * 24}
+                  style={{
+                    paddingLeft: PixelRatio.getFontScale() * 15,
+                    alignItems: "center",
+                    paddingTop: PixelRatio.getFontScale() * 3,
+                  }}
+                />
+              </View>
             </View>
 
-            <View style={styles.linhaHorizontal}></View>
-
-            <View style={styles.containerOR}/*Orientador Responsável*/>
-
-                <View style={styles.headerOR}>
-                    <View>
-                        <Text style={styles.title} >Orientador Responsável</Text>
-                    </View>
-                </View>
-
-                <View style={styles.containerEdit}>                    
-                    <Text style={styles.labelEdit}>Nome</Text>
-
-                    <View style={styles.boxInputOR}> 
-                        <Text style={styles.labelInput}>Fulano Fulano</Text>
-                    </View>
-                </View>
-                <View style={styles.containerEdit}>                    
-                    <Text style={styles.labelEdit}>Instituição</Text>
-
-                    <View style={styles.boxInputOR}> 
-                        <Text style={styles.labelInput}>UFSCar</Text>
-                    </View>
-                </View>
-                <View style={styles.containerEdit}>                    
-                    <Text style={styles.labelEdit}>Última avaliação</Text>
-
-                    <View style={styles.boxInputOR}> 
-                        <Text style={styles.labelInput}>30/10/2024</Text>
-                    </View>
-                </View>
-            
-
+            <View style={styles.containerEdit}>
+              <View style={styles.boxInputLong}>
+                <TextInput
+                  style={styles.labelInputBox}
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  placeholder="Faça alguma observação"
+                  value={form.descricao}
+                  onChangeText={(descricao) => setForm({ ...form, descricao })}
+                />
+              </View>
             </View>
-
-        </ScrollView>
+          </View>
+        </View>
 
         <View style={styles.linhaHorizontal}></View>
 
-        <View style={{paddingBottom: '10%'}}/*Confirmar envio*/>
-            <TouchableOpacity onPress={() => console.log("Confirmar Envio")} style={[styles.confirmaBox, {alignSelf:'center'}]}>
-                <Text style={[styles.labelConfirma]}>Confirmar Envio</Text>
-                <Feather name={'check'} color={'#374957'} size={PixelRatio.getFontScale()* 24} />
-            </TouchableOpacity>
-        </View>
+        <View style={styles.containerOR} /*Orientador Responsável*/>
+          <View style={styles.headerOR}>
+            <View>
+              <Text style={styles.title}>Orientador Responsável</Text>
+            </View>
+          </View>
 
+          <View style={styles.containerEdit}>
+            <Text style={styles.labelEdit}>Nome</Text>
+
+            <View style={styles.boxInputOR}>
+              <Text style={styles.labelInput}>Fulano Fulano</Text>
+            </View>
+          </View>
+          <View style={styles.containerEdit}>
+            <Text style={styles.labelEdit}>Instituição</Text>
+
+            <View style={styles.boxInputOR}>
+              <Text style={styles.labelInput}>UFSCar</Text>
+            </View>
+          </View>
+          <View style={styles.containerEdit}>
+            <Text style={styles.labelEdit}>Última avaliação</Text>
+
+            <View style={styles.boxInputOR}>
+              <Text style={styles.labelInput}>30/10/2024</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+
+      <View style={styles.linhaHorizontal}></View>
+
+      <View style={{ paddingBottom: "10%" }} /*Confirmar envio*/>
+        <TouchableOpacity
+          onPress={() => console.log("Confirmar Envio")}
+          style={[styles.confirmaBox, { alignSelf: "center" }]}
+        >
+          <Text style={[styles.labelConfirma]}>Confirmar Envio</Text>
+          <Feather
+            name={"check"}
+            color={"#374957"}
+            size={PixelRatio.getFontScale() * 24}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -166,109 +189,107 @@ export default function RelatorioOrientacao() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 
   title: {
     fontSize: 22,
-    paddingBottom:10,
-    fontWeight:'600',
+    paddingBottom: 10,
+    fontWeight: "600",
   },
-  label:{
+  label: {
     fontSize: PixelRatio.getFontScale() * 16,
-    fontWeight:'400',
-    color:'#2F2E2E',
+    fontWeight: "400",
+    color: "#2F2E2E",
   },
 
   // Linha Horizontal //
 
-  linhaHorizontal:{
+  linhaHorizontal: {
     height: 1.5,
-    backgroundColor: '#CDD2D3',
-    width:PixelRatio.getFontScale() * 340,
-    alignSelf:'center',
+    backgroundColor: "#CDD2D3",
+    width: PixelRatio.getFontScale() * 340,
+    alignSelf: "center",
     marginVertical: PixelRatio.getFontScale() * 25,
   },
 
   // Header //
-  
+
   header: {
-    flexDirection:'row',
-    width: '100%',
-    alignItems:'center',
-    justifyContent: 'space-around',
-    paddingTop: PixelRatio.getFontScale() * 25
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingTop: PixelRatio.getFontScale() * 25,
   },
 
   // Descrição Relatório //
 
-  linksHorizontais:{
-    flexDirection:'row',
-    width: '100%',
-    alignItems:'center',
-    justifyContent: 'space-around',
-    paddingBottom: PixelRatio.getFontScale() * 15
+  linksHorizontais: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingBottom: PixelRatio.getFontScale() * 15,
   },
 
-  containerEdit:{
-    flexDirection:'row',
-    width: '100%',
-    alignItems:'center',
-    justifyContent:'space-evenly',
-    paddingVertical: 10
+  containerEdit: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    paddingVertical: 10,
   },
 
-  labelEdit:{
+  labelEdit: {
     fontSize: PixelRatio.getFontScale() * 16,
-    fontWeight: '400',
-    color: '#2F2E2E',
-    textAlign: 'center',
-    width: PixelRatio.getFontScale() * 120
-    
+    fontWeight: "400",
+    color: "#2F2E2E",
+    textAlign: "center",
+    width: PixelRatio.getFontScale() * 120,
   },
 
   boxInput: {
-    backgroundColor: '#E6EAEB',
-    borderColor: '#374957',
-    flexDirection: 'row',
-    alignItems: 'center',
+    backgroundColor: "#E6EAEB",
+    borderColor: "#374957",
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 0.5,
     width: PixelRatio.getFontScale() * 200,
     paddingHorizontal: 10,
   },
 
-  boxInputLong:{
-    backgroundColor: '#f8f8fa',
-    borderColor: '#374957',
-    flexDirection: 'row',
-    alignSelf:'center',
+  boxInputLong: {
+    backgroundColor: "#f8f8fa",
+    borderColor: "#374957",
+    flexDirection: "row",
+    alignSelf: "center",
     borderWidth: 0.5,
-    width: '80%',
+    width: "80%",
     height: 200,
   },
-  
+
   labelInput: {
     fontSize: PixelRatio.getFontScale() * 16,
-    fontWeight: '400',
-    color: '#2F2E2E',
-    textAlign: 'center',
+    fontWeight: "400",
+    color: "#2F2E2E",
+    textAlign: "center",
     flex: 1,
   },
 
   // Orientador Responsavel //
 
   containerOR: {
-    width: '100%',
+    width: "100%",
   },
 
-  headerOR:{
-    paddingLeft: PixelRatio.getFontScale() * 25
+  headerOR: {
+    paddingLeft: PixelRatio.getFontScale() * 25,
   },
   boxInputOR: {
-    
-    borderColor: '#CDD2D3',
-    flexDirection: 'row',
-    alignItems: 'center',
+    borderColor: "#CDD2D3",
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 1,
     borderRadius: 5,
     width: PixelRatio.getFontScale() * 200,
@@ -277,87 +298,85 @@ const styles = StyleSheet.create({
 
   // Confirmar box //
 
-  confirmaBox:{
-    borderColor: '#CDD2D3',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-around',
+  confirmaBox: {
+    borderColor: "#CDD2D3",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
     borderWidth: 1,
-    width:PixelRatio.getFontScale()*220,
-    height:PixelRatio.getFontScale()*55,
-    borderRadius: 10
+    width: PixelRatio.getFontScale() * 220,
+    height: PixelRatio.getFontScale() * 55,
+    borderRadius: 10,
   },
-  labelConfirma:{
+  labelConfirma: {
     marginLeft: PixelRatio.getFontScale() * 5,
     fontSize: PixelRatio.getFontScale() * 20,
-    fontWeight:'600',
-    color:'#2F2E2E',
-    textAlign:'center'
+    fontWeight: "600",
+    color: "#2F2E2E",
+    textAlign: "center",
   },
 
   // Redirect box //
 
-  redirectBox:{
-    borderColor: '#CDD2D3',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-around',
+  redirectBox: {
+    borderColor: "#CDD2D3",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
     borderWidth: 1,
-    width:PixelRatio.getFontScale()*165,
-    height:PixelRatio.getFontScale()*50,
-    borderRadius: 20
+    width: PixelRatio.getFontScale() * 165,
+    height: PixelRatio.getFontScale() * 50,
+    borderRadius: 20,
   },
-  labelRedirect:{
+  labelRedirect: {
     marginLeft: PixelRatio.getFontScale() * 5,
     fontSize: PixelRatio.getFontScale() * 16,
-    fontWeight:'600',
-    color:'#2F2E2E',
-    textAlign:'center'
+    fontWeight: "600",
+    color: "#2F2E2E",
+    textAlign: "center",
   },
-
 
   // Select Box //
 
-  selectBox:{
-    width:PixelRatio.getFontScale() * 150,
-    backgroundColor:'#D9D9D9',
-    alignItems:'center',
-    height: PixelRatio.getFontScale() * 65
+  selectBox: {
+    width: PixelRatio.getFontScale() * 150,
+    backgroundColor: "#D9D9D9",
+    alignItems: "center",
+    height: PixelRatio.getFontScale() * 65,
   },
 
-  selectLabel:{
+  selectLabel: {
     fontSize: PixelRatio.getFontScale() * 16,
-    fontWeight:'400',
-    color:'#2F2E2E',
-    textAlign:'center',
+    fontWeight: "400",
+    color: "#2F2E2E",
+    textAlign: "center",
   },
-
 
   // Edit Box //
 
-  editBox:{
-    borderColor: '#CDD2D3',
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-around',
+  editBox: {
+    borderColor: "#CDD2D3",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
     borderWidth: 1.5,
-    width:PixelRatio.getFontScale()*165,
-    height:PixelRatio.getFontScale()*50,
+    width: PixelRatio.getFontScale() * 165,
+    height: PixelRatio.getFontScale() * 50,
   },
 
-  labelBoxEdit:{
+  labelBoxEdit: {
     marginLeft: PixelRatio.getFontScale() * 5,
     fontSize: PixelRatio.getFontScale() * 16,
-    fontWeight:'400',
-    color:'#2F2E2E',
-    textAlign:'center'
+    fontWeight: "400",
+    color: "#2F2E2E",
+    textAlign: "center",
   },
   labelInputBox: {
     fontSize: PixelRatio.getFontScale() * 16,
-    fontWeight: '400',
-    color: '#2F2E2E',
-    textAlign: 'justify',
-    textAlignVertical:'top',
-    padding:PixelRatio.getFontScale() * 10,    
+    fontWeight: "400",
+    color: "#2F2E2E",
+    textAlign: "justify",
+    textAlignVertical: "top",
+    padding: PixelRatio.getFontScale() * 10,
   },
 });
